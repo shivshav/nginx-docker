458a7caebe03c556d03c869a6127ff1c8c6412cc
shiv <sprasad0603@gmail.com>
2016-02-04 00:41:21 -0800

    Removing the destroy errors

--- destroyNginx.sh
+++ destroyNginx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 NGINX_NAME=${NGINX_NAME:-proxy}
-docker stop ${NGINX_NAME}
-docker rm -v ${NGINX_NAME}
+
+echo "Removing ${NGINX_NAME}..."
+docker stop ${NGINX_NAME} &> /dev/null
+docker rm -v ${NGINX_NAME} &> /dev/null
